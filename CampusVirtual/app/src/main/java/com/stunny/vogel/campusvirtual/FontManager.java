869e3bacc3e8e6a048c3a5b0a1480218b97146a0
Alex Vogel <avogelm@gmail.com>
2016-05-25 18:06:33 +0200

    Starting Welcome Activity

---
+++ CampusVirtual/app/src/main/java/com/stunny/vogel/campusvirtual/FontManager.java
@@ -0,0 +1,13 @@
+package com.stunny.vogel.campusvirtual;
+
+/**
+ * Created by alex on 25/05/16.
+ */
+public class FontManager {
+
+
+    public final String ROOT = "fonts/";
+    OCTICONS = ROOT + "octicons-local.ttf";
+
+
+}
--- CampusVirtual/app/src/main/java/com/stunny/vogel/campusvirtual/WelcomeActivity.java
+++ CampusVirtual/app/src/main/java/com/stunny/vogel/campusvirtual/WelcomeActivity.java
@@ -1,7 +1,11 @@
 package com.stunny.vogel.campusvirtual;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        //--Hacemos que la actividad de bienvenida se muestre en pantalla completa.--//
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+
+        //--Cargamos la fuente del logo al TextView con id "logo"--//
+        Typeface font = Typeface.createFromAsset(;
+        TextView logo = (TextView) findViewById(R.id.logo);
+        logo.setTypeface(font);
+        //logo.setTextColor(R.color.logoColor);
+
     }
 }
