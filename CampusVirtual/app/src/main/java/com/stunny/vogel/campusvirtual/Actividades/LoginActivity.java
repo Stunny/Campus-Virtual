f904e832143502ca327fe9eb5a55ab90b2ae1e39
Alex Vogel <avogelm@gmail.com>
2016-05-26 14:00:26 +0200

    Proceso de login correcto y funcionando

--- CampusVirtual/app/src/main/java/com/stunny/vogel/campusvirtual/Actividades/LoginActivity.java
+++ CampusVirtual/app/src/main/java/com/stunny/vogel/campusvirtual/Actividades/LoginActivity.java
@@ -1,19 +1,20 @@
 package com.stunny.vogel.campusvirtual.Actividades;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.stunny.vogel.campusvirtual.R;
 
 public class LoginActivity extends AppCompatActivity {
 
 
-    EditText _emailInput = (EditText) findViewById(R.id.email_input),
-             _passwordInput = (EditText) findViewById(R.id.password_input);
-    Button   _loginButton = (Button) findViewById(R.id.btn_log);
 
 
 
@@ -22,16 +23,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         //--ESCONDEMOS LA ACTION BAR--//
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        getSupportActionBar().hide();
 
         //-- Escuchamos al boton de entrar --//
+        Button _loginButton = (Button) findViewById(R.id.btn_log);
+
         _loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +40,105 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void login() {
+
+        EditText _emailInput = (EditText) findViewById(R.id.email_input);
+        EditText _passwordInput = (EditText) findViewById(R.id.password_input);
+        Button _loginButton = (Button) findViewById(R.id.btn_log);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this, ProgressDialog.STYLE_SPINNER);
+
+
+        if (!validate()) {
+            onLoginFailed(progressDialog);
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Autenticando...");
+        progressDialog.show();
+
+        String email = _emailInput.getText().toString();
+        String password = _passwordInput.getText().toString();
+
+        if(!email.equals(getString(R.string.user_email)) || !password.equals(getString(R.string.user_password))){
+            if(!email.equals(getString(R.string.user_email))){
+                _emailInput.setError(getString(R.string.wrong_email));
+            }
+            if(!email.equals(getString(R.string.user_password))){
+                _passwordInput.setError(getString(R.string.wrong_pass));
+            }
+            onLoginFailed(progressDialog);
+            return;
+        }
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        Button _loginButton = (Button) findViewById(R.id.btn_log);
+        _loginButton.setEnabled(true);
+
+        Intent i = new Intent(LoginActivity.this, MainMenuActivity.class);
+        startActivity(i);
+
+        finish();
+    }
+
+    public void onLoginFailed(ProgressDialog pd) {
+        Button _loginButton = (Button) findViewById(R.id.btn_log);
+        pd.dismiss();
+
+        _loginButton.setEnabled(true);
+    }
+    public boolean validate(){
+
+        boolean ok = true;
+
+        EditText _emailInput = (EditText) findViewById(R.id.email_input);
+        EditText _passwordInput = (EditText) findViewById(R.id.password_input);
+
+        String email = _emailInput.getText().toString(),
+               password = _passwordInput.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+
+            _emailInput.setError("Introduce una dirección de email");
+            ok = false;
+
+        } else {
+
+            _emailInput.setError(null);
+
+        }
+
+        if (password.isEmpty() || password.length() < 6) {
+
+            _passwordInput.setError("La contraseña debe ser de mínimo 6 carácteres.");
+            ok = false;
+
+        } else {
+
+            _passwordInput.setError(null);
+
+        }
+
+        return ok;
 
     }
 
--- CampusVirtual/app/src/main/java/com/stunny/vogel/campusvirtual/Actividades/WelcomeActivity.java
+++ CampusVirtual/app/src/main/java/com/stunny/vogel/campusvirtual/Actividades/WelcomeActivity.java
@@ -23,14 +23,7 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         //--Hacemos que la actividad de bienvenida se muestre en pantalla completa.--//
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
+        getSupportActionBar().hide();
 
         //--Asignamos el ic_launcher al ImageView del logo --//
         ImageView logo = (ImageView)findViewById(R.id.logo);
